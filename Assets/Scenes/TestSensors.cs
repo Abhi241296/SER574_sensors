ee7414438647525196f174f8251c87a5573fceea
aneeshdalvi <adalvi1@asu.edu>
2020-02-25 18:25:10 -0700

    Added unity project for testing sensors.dll

---
+++ Assets/Scenes/TestSensors.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Sensors;
+
+public class TestSensors : MonoBehaviour
+{
+
+    public Button play;
+    // Start is called before the first frame update
+    void Start()
+    {
+        play.onClick.AddListener(callSensorLibrary);
+    }
+
+    public void callSensorLibrary()
+    {
+        Sensors.Sensors sensor = new Sensors.Sensors();
+        Debug.Log(sensor.printSensors());
+    }
+}
